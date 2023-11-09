95ea74903d70d5c2215e06bba4cf9d1d5e50f364
Alireza Feyzabadi <arfa79lg@gmail.com>
2023-11-09 23:06:18 +0330

    django-environ comment

--- Djangjoo_project/settings.py
+++ Djangjoo_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import environ
+import environ #Please notice the django-environ and environ are two diffrent packages and we need django-environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
